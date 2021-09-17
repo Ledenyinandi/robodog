0e9014a7f15f5a09624cd1c8b5af2d7886c3522c
Ledenyinandi <ledenyinandi@gmail.com>
2021-09-17 18:29:57 +0200

    Initial commit

---
+++ src/test/java/nandor/ledenyi/robodog/RobodogApplicationTests.java
@@ -0,0 +1,13 @@
+package nandor.ledenyi.robodog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobodogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
